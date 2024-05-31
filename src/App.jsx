111861e8c47d8617ca0b87ba54016dd23a7abb22
svivek19 <svivek030503@gmail.com>
2024-05-31 12:17:11 +0530

    logic finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./Data";
+import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const handlePrev = () => {
+    if (imageIndex <= 0) {
+      setImageIndex(data.length - 1);
+    } else {
+      setImageIndex(imageIndex - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (imageIndex >= data.length - 1) {
+      setImageIndex(0);
+    } else {
+      setImageIndex(imageIndex + 1);
+    }
+  };
+
+  return (
+    <div className="main">
+      <button onClick={handlePrev}>{"<"}</button>
+      <img src={data[imageIndex]} alt="image" />
+      <button onClick={handleNext}>{">"}</button>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Data.jsx
@@ -0,0 +1,8 @@
+const data = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3oAa216dhvSkp1YJ0djbJl6AL8oWF5vB9aA&s",
+  "https://img.freepik.com/free-photo/snowy-mountain-peak-starry-galaxy-majesty-generative-ai_188544-9650.jpg",
+  "https://wallpapers.com/images/featured/nature-2ygv7ssy2k0lxlzu.jpg",
+  "https://c4.wallpaperflare.com/wallpaper/974/565/254/windows-11-windows-10-minimalism-hd-wallpaper-thumb.jpg",
+];
+
+export default data;
